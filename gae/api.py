1ad1c5b8430e2cd107d3951fb335cac405a50330
skxeve <skxeve@gmail.com>
2019-02-18 20:08:19 +0900

    create sleep api

--- gae/api.py
+++ gae/api.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, current_app, jsonify, Flask
+from flask import Blueprint, current_app, jsonify, Flask, make_response
+from time import sleep
 
 var_count = 999
 
@@ -14,7 +15,26 @@ def hello():
     })
 
 
+@api.route('/sleep/<int:sec>')
+def sleep_logging(sec):
+    if sec > 120:
+        per = 10
+    elif sec > 50:
+        per = 5
+    else:
+        per = 1
+    for i in range(sec):
+        sleep(1)
+        if i % per == per - 1:
+            current_app.logger.info("sleeping {}/{} sec.".format(i + 1, sec))
+    result = {
+        "result": True,
+        "sec": sec,
+    }
+    return make_response(jsonify(result))
+
+
 if __name__ == "__main__":
     app = Flask(__name__)
-    app.register_blueprint(api)
+    app.register_blueprint(api, url_prefix="/api/v1")
     app.run(host='0.0.0.0', port=8080, debug=True)
